299af65a9d69ff11a540e6211b91203377205eec
jacquelinegarrahan <jacqueline.garrahan@aiera.com>
2024-05-01 22:28:16 -0700

    Check in wavefunction scripts

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # animation
 Animation repo
+
+
+## Set up the repo
+```bash
+conda env create -f environment.yml
+conda activate animation
+```
+
+## Run script
+```bash
+python wavefunction_sampling_pairs {filename(w/o .mp4)} {n pixels}
+```
+
+
+For example:
+```bash
+python wavefunction_sampling_pairs test 100
+```
+
+The file will be saved as text.mp4
--- wavefunction_sampling_pairs.py
+++ wavefunction_sampling_pairs.py
@@ -12,7 +12,11 @@ def get_color(hue):
     # Convert hue to RGB using matplotlib
     return plt.cm.hsv(hue/360)  # Normalized hue to [0, 1]
 
-def generate_frame(t, N, entanglement_phase):
+
+
+
+
+def generate_frame_y_symmetry(t, N, entanglement_phase):
     frame = np.zeros((N, N, 3))
     for i in range(N):
         for j in range(N):
@@ -27,23 +31,34 @@ def generate_frame(t, N, entanglement_phase):
     return frame
 
 def update_plot(frame_number, N, entanglement_phase, img):
-    frame = generate_frame(frame_number * np.pi / 5, N, entanglement_phase)
+    frame = generate_frame_y_symmetry(frame_number * np.pi / 5, N, entanglement_phase)
     img.set_array(frame)
     return img
 
-N = 100  # Image resolution (100x100 pixels)
-fig, ax = plt.subplots()
+def main(filename, N, frames=100, interval=100):
+    #N = 100  # Image resolution (100x100 pixels)
+    fig, ax = plt.subplots()
 
-# Set aspect ratio and remove axes
-ax.axis('off')
+    # Set aspect ratio and remove axes
+    ax.axis('off')
 
-# Adjust figure margins
-fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    # Adjust figure margins
+    fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
-img_plot = ax.imshow(np.zeros((N, N, 3)), interpolation='nearest', aspect='auto')
+    img_plot = ax.imshow(np.zeros((N, N, 3)), interpolation='nearest', aspect='auto')
 
-ani = FuncAnimation(fig, update_plot, frames=100, fargs=(N, np.pi/4, img_plot), interval=100)
+    ani = FuncAnimation(fig, update_plot, frames=frames, fargs=(N, np.pi/4, img_plot), interval=interval)
 
-# Save the animation as an MP4
-ani.save('wavefunction_animation.mp4', writer='ffmpeg', fps=10)
-plt.close()
+    # Save the animation as an MP4
+    ani.save(f'{filename}.mp4', writer='ffmpeg', fps=10)
+    plt.close()
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process a filename.')
+    parser.add_argument('filename', type=str, help='The path')
+    parser.add_argument('N',type=int, help="N pixels")
+    args = parser.parse_args()
+
+    main(args.filename, args.N)
